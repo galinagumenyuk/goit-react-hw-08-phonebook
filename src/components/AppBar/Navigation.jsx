854d754362fa05953c4d7ed8753d0e8542f37258
galinagumenyuk <gymenyuk.galina@gmail.com>
2022-02-07 15:35:56 +0200

    forms

--- src/components/AppBar/Navigation.jsx
+++ src/components/AppBar/Navigation.jsx
@@ -6,6 +6,8 @@ const Navigation = () => {
         <nav className={s.nav }>
             <NavLink to='/' className={navData => navData.isActive ? s.active : s.link}>Home</NavLink>
             <NavLink to='/contacts' className={navData => navData.isActive ? s.active : s.link}>Contacts</NavLink>
+            <NavLink to='/login' className={navData => navData.isActive ? s.active : s.link}>Log In</NavLink>
+            <NavLink to='/register' className={navData => navData.isActive ? s.active : s.link}>Sign Up</NavLink>
     </nav>
     )}
 
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,55 @@
 import React from "react";
+import { useState } from "react";
 
 const Login = () => { 
-    return (
-        <div>Login</div>
-    )
+        const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleChange = (e) => {
+        switch (e.target.name) {
+            case "name":
+                setName(e.target.value);
+             break;
+            case "email":
+                setEmail(e.target.value);
+            break;
+            default:
+                return;
+        }
+    };
+
+    const onHandleSubmit = (e) => { 
+         e.preventDefault();
+    }
+  
+    return (<>
+      <h2>Welcome back!</h2>
+    <form onSubmit={onHandleSubmit}>
+      <label>
+        name
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        email
+        <input
+          type="email"
+          name="email"
+          required
+          value={email}
+          onChange={handleChange}
+        />
+      </label>
+     <button type="button">qwe</button>
+      </form>
+      </>
+  );
 }
 export default Login;
